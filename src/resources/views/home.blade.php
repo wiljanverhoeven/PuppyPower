504f97ee776560fb0136682a9065add201befd75
Kevin <kvindso@gmail.com>
2025-06-10 17:04:44 +0200

    header image photo collection

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,11 +1,23 @@
 <x-app-layout>
     <div class="">
         {{-- header image --}}
-        <div class="relative h-96">
-            <img src="{{ asset('/images/header-placeholder.png') }}" alt="Header Image" class="w-full h-full object-cover brightness-60">
-            <div class="absolute inset-0 flex flex-col items-center justify-center text-center p-4">
-                <h1 class="text-gray-100 text-4xl font-bold">Welkom bij Puppy Power Academy</h1>
-                <p class="text-gray-100">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <div class="relative h-[calc(100vh-4rem)] border-solid border-2 border-red-600 p-6">
+            {{-- upper row --}}
+            <div class="border-solid border-2 border-blue-600 h-1/3 flex justify-between items-center gap-2 mb-2">
+                <div class="border-solid border-2 border-red-600 h-full w-2/3"></div>
+                <div class="border-solid border-2 border-red-600 h-full w-1/3"></div>
+            </div>
+            {{-- middle row --}}
+            <div class="border-solid border-2 border-blue-600 h-1/3 flex justify-between items-center gap-2 mb-2">
+                <div class="border-solid border-2 border-red-600 h-full w-1/4"></div>
+                <div class="border-solid border-2 border-red-600 h-full w-1/4"></div>
+                <div class="border-solid border-2 border-red-600 h-full w-1/4"></div>
+                <div class="border-solid border-2 border-red-600 h-full w-1/4"></div>
+            </div>
+            {{-- bottom row --}}
+            <div class="border-solid border-2 border-blue-600 h-1/3 flex justify-between items-center gap-2">
+                <div class="border-solid border-2 border-red-600 h-full w-1/3"></div>
+                <div class="border-solid border-2 border-red-600 h-full w-2/3"></div>
             </div>
         </div>
 
